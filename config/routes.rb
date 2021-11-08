d81445394c7fe9a419b4f1ca2ab4782e193978e2
yuko.iba <exast.06.ong@outlook.jp>
2021-11-08 15:18:10 +0000

    タイムラインと多対多

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+      get :search
+  end
+
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
